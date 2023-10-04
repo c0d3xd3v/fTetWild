4fb71d525e364ab7a81c2655daa8439c08444bc6
Kai Henning <khenning@math.tu-berlin.de>
2023-10-04 19:35:29 +0200

    add install libs

--- installLibs.cmake
+++ installLibs.cmake
@@ -1,5 +1,5 @@
 message(STATUS "Try to install Tetwild libs from ${CMAKE_CURRENT_BINARY_DIR}")
-FILE(GLOB_RECURSE lib_list *.a)
+FILE(GLOB_RECURSE lib_list *.lib)
 list(LENGTH lib_list lib_list_count)
 FOREACH(_LIB ${lib_list})
     get_filename_component(FN ${_LIB} NAME)
